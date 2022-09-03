f24cfb66f96ea10addb8af94e326d1495bd86685
bennie10colado <davy.eugenio@hotmail.com.com>
2022-09-03 00:31:16 -0300

    AlternativaResource e Metodos de Alternativa em facade

--- src/main/java/br/com/jogo/facade/Jogo.java
+++ src/main/java/br/com/jogo/facade/Jogo.java
@@ -6,7 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import br.com.jogo.domain.Alternativa;
+import br.com.jogo.domain.Categoria;
 import br.com.jogo.domain.Questao;
+import br.com.jogo.dto.AlternativaNewDTO;
+import br.com.jogo.dto.CategoriaNewDTO;
 import br.com.jogo.dto.QuestaoDTO;
 import br.com.jogo.dto.QuestaoNewDTO;
 import br.com.jogo.services.AlternativaService;
@@ -47,4 +51,52 @@ public class Jogo {
 		obj.setCategoria(categoriaService.find(objDto.getCategoria().getId()));
 		return questaoService.update(obj);
 	}
-}
+	
+	//--------------------------------Alternativa----------------------------------------------
+	
+	public Alternativa insertAlternativa(AlternativaNewDTO objDto) {
+		Alternativa obj = alternativaService.fromDTO(objDto);
+		return alternativaService.insert(obj);
+	}
+	
+	public Alternativa findAlternativa(Integer id) {
+		return alternativaService.find(id);
+	}
+	
+	public List<Alternativa> findAllAlternativa() {
+		return alternativaService.findAll();
+	}
+	
+	public void deleteAlternativa(Integer id) {
+		alternativaService.delete(id);
+	}
+	
+	public Alternativa updateAlternativa(Alternativa obj) {
+		return alternativaService.update(obj);
+	}
+	
+	//--------------------------------Categoria----------------------------------------------
+	
+	public Categoria insertCategoria(CategoriaNewDTO objDto) {
+		Categoria obj = categoriaService.fromDTO(objDto);
+		return categoriaService.insert(obj);
+	}
+	
+	public Categoria findCategoria(Integer id) {
+		return categoriaService.find(id);
+	}
+	
+	public List<Categoria> findAllCategorias() {
+		return categoriaService.findAll();
+	}
+
+	public void deleteCategoria(Integer id) {
+		categoriaService.delete(id);
+	}/*
+
+	public Categoria updateCategoria(CategoriaDTO objDto) {
+		Categoria obj = categoriaService.fromDTO(objDto);
+		obj.setCategoria(categoriaService.find(objDto.getCategoria().getId()));
+		return categoriaService.update(obj);
+	}*/
+}	
---
+++ src/main/java/br/com/jogo/resources/AlternativaResource.java
@@ -0,0 +1,59 @@
+package br.com.jogo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.jogo.domain.Alternativa;
+import br.com.jogo.dto.AlternativaNewDTO;
+import br.com.jogo.facade.Jogo;
+
+@RestController
+@RequestMapping(path = "/alternativas")
+public class AlternativaResource {
+	@Autowired
+	Jogo jogo;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Alternativa> find(@PathVariable Integer id) {
+		Alternativa obj = jogo.findAlternativa(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Integer> insert(@Valid @RequestBody AlternativaNewDTO objNewDto) {
+		Alternativa obj = jogo.insertAlternativa(objNewDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj.getId());
+	}/*
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody AlternativaDTO objDto, @PathVariable Integer id) {
+		jogo.updateAlternativa(objDto);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		jogo.deleteAlternativa(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<AlternativaDTO>> findAll() {
+		List<Alternativa> list = jogo.findAllQuestoes();
+		List<AlternativaDTO> listDto = list.stream().map(obj -> new AlternativaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}*/
+}
